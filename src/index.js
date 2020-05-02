daa68ba2128e2bb85032cb6e668805a287d1d0f4
al feratovic <aladin.feratovic@gmail.com>
2020-05-01 22:19:39 -0400

    new commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Params
+let mainSliderSelector = '.main-slider',
+    navSliderSelector = '.nav-slider',
+    interleaveOffset = 0.5;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Main Slider
+let mainSliderOptions = {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000
+    },
+    loopAdditionalSlides: 10,
+    grabCursor: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        init: function () {
+            this.autoplay.stop();
+        },
+        imagesReady: function () {
+            this.el.classList.remove('loading');
+            this.autoplay.start();
+        },
+        slideChangeTransitionEnd: function () {
+            let swiper = this,
+                captions = swiper.el.querySelectorAll('.caption');
+            for (let i = 0; i < captions.length; ++i) {
+                captions[i].classList.remove('show');
+            }
+            swiper.slides[swiper.activeIndex].querySelector('.caption').classList.add('show');
+        },
+        progress: function () {
+            let swiper = this;
+            for (let i = 0; i < swiper.slides.length; i++) {
+                let slideProgress = swiper.slides[i].progress,
+                    innerOffset = swiper.width * interleaveOffset,
+                    innerTranslate = slideProgress * innerOffset;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+                swiper.slides[i].querySelector(".slide-bgimg").style.transform =
+                    "translateX(" + innerTranslate + "px)";
+            }
+        },
+        touchStart: function () {
+            let swiper = this;
+            for (let i = 0; i < swiper.slides.length; i++) {
+                swiper.slides[i].style.transition = "";
+            }
+        },
+        setTransition: function (speed) {
+            let swiper = this;
+            for (let i = 0; i < swiper.slides.length; i++) {
+                swiper.slides[i].style.transition = speed + "ms";
+                swiper.slides[i].querySelector(".slide-bgimg").style.transition =
+                    speed + "ms";
+            }
+        }
+    }
+};
+let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
+
+// Navigation Slider
+let navSliderOptions = {
+    loop: true,
+    loopAdditionalSlides: 10,
+    speed: 1000,
+    spaceBetween: 5,
+    slidesPerView: 5,
+    centeredSlides: true,
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+    direction: 'vertical',
+    on: {
+        imagesReady: function () {
+            this.el.classList.remove('loading');
+        },
+        click: function () {
+            mainSlider.autoplay.stop();
+        }
+    }
+};
+let navSlider = new Swiper(navSliderSelector, navSliderOptions);
+
+// Matching sliders
+mainSlider.controller.control = navSlider;
+navSlider.controller.control = mainSlider;
