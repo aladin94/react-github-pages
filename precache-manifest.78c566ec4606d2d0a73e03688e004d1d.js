ffaf9d3bfc76b54bc35c7d2137cc31ddd7dcc0e5
al feratovic <aladin.feratovic@gmail.com>
2020-05-01 22:09:40 -0400

    Updates

---
+++ precache-manifest.78c566ec4606d2d0a73e03688e004d1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4aab7522a972508ad37de724d42a500",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "5b9c4ced2f2e58641ae5",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b5496d01683aa3cb7adf",
+    "url": "/react-gh-pages/static/js/2.c20afc65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.c20afc65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9c4ced2f2e58641ae5",
+    "url": "/react-gh-pages/static/js/main.ead41fe9.chunk.js"
+  },
+  {
+    "revision": "a997256c282cacfc5fcd",
+    "url": "/react-gh-pages/static/js/runtime-main.0ff5cf3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
